c080c72072e23e15b3dd1e280c7ac09afd586aac
mkhuda <mhmmd.huda@gmail.com>
2018-04-13 03:16:24 +0700

    knex

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
   development: {
     client: 'mysql2',
     connection: {
-      host: 'mysql://bd71451ac8b6f0:a5221c32@us-cdbr-iron-east-05.cleardb.net/?reconnect=true',
+      host: 'bd71451ac8b6f0:a5221c32@us-cdbr-iron-east-05.cleardb.net',
       database: 'heroku_00575bb32b9053c',
       user:     'bd71451ac8b6f0',
       password: '	a5221c32'
@@ -22,7 +22,7 @@ module.exports = {
   production: {
     client: 'mysql2',
     connection: {
-      host: 'mysql://bd71451ac8b6f0:a5221c32@us-cdbr-iron-east-05.cleardb.net/?reconnect=true',
+      host: 'bd71451ac8b6f0:a5221c32@us-cdbr-iron-east-05.cleardb.net',
       database: 'heroku_00575bb32b9053c',
       user:     'bd71451ac8b6f0',
       password: '	a5221c32'
