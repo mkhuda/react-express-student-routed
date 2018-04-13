5c116d832134455476107cd45c0f8663f9a54042
mkhuda <mhmmd.huda@gmail.com>
2018-04-13 07:59:02 +0700

    use JawsDB Heroku

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
 
   development: {
     client: 'mysql2',
-    connection: 'mysql://xnou6erwx6nl77v1:kejp0l537njxszck@l6slz5o3eduzatkw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ozbp2iek3q6isj23',
+    connection: process.env.HEROKU_JAWSDB_MYSQL,
     pool: {
       min: 2,
       max: 10
@@ -16,7 +16,7 @@ module.exports = {
 
   production: {
     client: 'mysql2',
-    connection: 'mysql://xnou6erwx6nl77v1:kejp0l537njxszck@l6slz5o3eduzatkw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ozbp2iek3q6isj23',
+    connection: process.env.HEROKU_JAWSDB_MYSQL,
     pool: {
       min: 2,
       max: 10
